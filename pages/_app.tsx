b2fbe071738101a8cad29d5187ac86c70055ec5e
raw <xogh2242@gmail.com>
2025-10-10 23:43:11 +0900

    globals.css 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 // pages/_app.tsx
 import type { AppProps } from 'next/app';
+import '../app/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
